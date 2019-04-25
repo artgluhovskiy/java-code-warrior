602fa2fe5a40b462bd12294ebb35bc3156a5278e
Artsiom Hlukhouski <gluhovskiy.art@gmail.com>
2019-04-25 18:30:37 +0300

    [JCW] Commons Module - Add Gradle Wrapper to the git repo, exclude Spring Boot Gradle plugin from the module

--- commons/src/main/java/org/art/web/warrior/commons/CommonsApplication.java
+++
@@ -1,9 +0,0 @@
-package org.art.web.warrior.commons;
-
-import org.springframework.boot.SpringApplication;
-
-public class CommonsApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CommonsApplication.class, args);
-    }
-}
