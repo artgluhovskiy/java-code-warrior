fc6cde37e2a86eb396b2fadf98de7d534b931c3f
Artsiom Hlukhouski <gluhovskiy.art@gmail.com>
2019-06-28 17:54:18 +0300

    [User Service] Enable Hibernate logging; minor code changes

--- compiler-service/src/main/java/org/art/web/warrior/compiler/CompilationServiceApplication.java
+++ compiler-service/src/main/java/org/art/web/warrior/compiler/CompilationServiceApplication.java
@@ -2,8 +2,10 @@ package org.art.web.warrior.compiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompilationServiceApplication {
 
     public static void main(String[] args) {
--- task-service/src/main/java/org/art/web/warrior/tasking/TaskServiceApplication.java
+++ task-service/src/main/java/org/art/web/warrior/tasking/TaskServiceApplication.java
@@ -1,11 +1,11 @@
 package org.art.web.warrior.tasking;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
